fbebf9a764eab1bc061bf297ac5dd1ed9460eaa5
Azra Muratovic <azra.muratovic@infobip.com>
2024-02-20 10:47:39 +0100

    Initial commit

---
+++ src/main/java/com/example/first_spring_ib/FirstSpringIbApplication.java
@@ -0,0 +1,13 @@
+package com.example.first_spring_ib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringIbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringIbApplication.class, args);
+	}
+
+}
