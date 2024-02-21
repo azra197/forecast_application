1656ab78dd9fb837f4137f4337dc72483151ed9e
Azra Muratovic <azra.muratovic@infobip.com>
2024-02-21 10:15:35 +0100

    Added PostMapping with Date and User info. Changed web server from Tomcat to Jetty.

--- src/main/java/com/example/first_spring_ib/UserController.java
+++ src/main/java/com/example/first_spring_ib/UserController.java
@@ -6,11 +6,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class UserController {
 
+    User user;
+
     @GetMapping("/math/sum")
     public int sum(@RequestParam (name="a") int no1, @RequestParam ("b") int no2) {
         return no1+no2;
     }
+
+    @PostMapping("/generate/model")
+    @ResponseBody
+    public DateUser person(@RequestParam int day, @RequestParam int month, @RequestParam int year, @RequestBody User user){
+        DateUser du = new DateUser();
+        du.setUser(user);
+        du.setDate(day+"."+month+"."+year);
+        return du;
+    }
 }
+
 
 
 
