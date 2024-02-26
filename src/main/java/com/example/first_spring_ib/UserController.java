d12e3c7eb68b514ad5abd33f73aac6476467d803
Azra Muratovic <azra.muratovic@infobip.com>
2024-02-26 11:04:33 +0100

    Added endpoint /forecast, which gives max,min and avg temperature and condition for 3 days

--- src/main/java/com/example/first_spring_ib/UserController.java
+++ src/main/java/com/example/first_spring_ib/UserController.java
@@ -9,26 +9,55 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping
 public class UserController {
 
-    User user;
+//    User user;
 
-    @GetMapping("/math/sum")
-    public int sum(@RequestParam(name = "a") int no1, @RequestParam("b") int no2) {
-        return no1 + no2;
+//    @GetMapping("/api/math/sum")
+//    public int sum(@RequestParam(name = "a") int no1, @RequestParam("b") int no2) {
+//        return no1 + no2;
+//    }
+//
+//    @PostMapping("/api/generate/model")
+//    @ResponseBody
+//    public DateUser person(@RequestParam int day, @RequestParam int month, @RequestParam int year, @RequestBody User user) {
+//        DateUser du = new DateUser();
+//        du.setUser(user);
+//        du.setDate(day + "." + month + "." + year);
+//        return du;
+//    }
+
+    @GetMapping("/forecast")
+    public List<String> forecastForThreeDays() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://api.weatherapi.com/v1/forecast.json?key=727bb15bfd014c1487795606242102&q=Sarajevo&days=3&aqi=no&alerts=no"))
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        ObjectMapper objectMapper = new ObjectMapper();
+        WeatherResponse weatherresponse = objectMapper.readValue(response.body(), WeatherResponse.class);
+
+        List<String>listForThreeDays = new ArrayList<>();
+        for(Days data : weatherresponse.getForecast().getForecastday()) {
+            String dataForcast = "Forecast for " + data.getDate() + " ,Max temeprature: " + data.getDay().getMaxtemp_c()
+                    + " ,Min temeprature: " + data.getDay().getMintemp_c() + " ,Average temeprature: " + data.getDay().getAvgtemp_c()
+                    + " ,Condition: " + data.getDay().getCondition().getText();
+            listForThreeDays.add(dataForcast);
+        }
+        return listForThreeDays;
+
     }
 
-    @PostMapping("/generate/model")
-    @ResponseBody
-    public DateUser person(@RequestParam int day, @RequestParam int month, @RequestParam int year, @RequestBody User user) {
-        DateUser du = new DateUser();
-        du.setUser(user);
-        du.setDate(day + "." + month + "." + year);
-        return du;
-    }
+
 
 
 }
